d2e3fb69b7b5d7cc9a356ef4a89b7a776fc1a60a
Andrew Dekker <simultech@gmail.com>
2015-05-20 23:04:12 +1000

    Initial Commit

---
+++ couch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  couch
+//
+//  Created by Andrew Dekker on 20/05/2015.
+//  Copyright (c) 2015 Andrew Dekker. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ couch/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  couch
+//
+//  Created by Andrew Dekker on 20/05/2015.
+//  Copyright (c) 2015 Andrew Dekker. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
